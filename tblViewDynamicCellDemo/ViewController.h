f09fec2ea0bfc8a84df29508a9534627877bb968
C-109 <c109@C109s-Mac-Pro.local>
2015-01-02 17:52:08 +0530

    commit

--- tblViewDynamicCellDemo/ViewController.h
+++ tblViewDynamicCellDemo/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  tblViewDynamicCellDemo
+//  tableViewCellResize
 //
 //  Created by C-109 on 02/01/15.
 //  Copyright (c) 2015 C109. All rights reserved.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tblView;
 
 @end
 
--- tblViewDynamicCellDemo/ViewController.m
+++ tblViewDynamicCellDemo/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  tblViewDynamicCellDemo
+//  tableViewCellResize
 //
 //  Created by C-109 on 02/01/15.
 //  Copyright (c) 2015 C109. All rights reserved.
@@ -11,17 +11,58 @@
 @interface ViewController ()
 
 @end
-
+int selected; // this will contain the selected cell index
+float ht;     // this will be your custom height
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    selected = -1;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    return cell;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
 
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.row != selected)
+        return 60;
+    else
+        return ht;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    CGRect rect = cell.frame;
+    rect.size.height = 150;
+    ht = 150;
+    selected = indexPath.row;
+    [cell setFrame:rect];
+    [tableView beginUpdates];
+    [tableView endUpdates];
+}
 @end
